6f305970d8f59df5bc41f30f216a1d945d688460
Agnès Haasser <agnes@hashbang.coop>
2024-10-10 17:06:46 +0200

    Add Admin Classes for DS new models

--- gsl_demarches_simplifiees/admin.py
+++ gsl_demarches_simplifiees/admin.py
@@ -1,1 +1,18 @@
-# Register your models here.
+from django.contrib import admin
+
+from gsl_demarches_simplifiees.models import Demarche, Dossier, FieldMappingForHuman
+
+
+@admin.register(Demarche)
+class DemarcheAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Dossier)
+class DossierAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(FieldMappingForHuman)
+class FieldMappingForHumanAdmin(admin.ModelAdmin):
+    pass
