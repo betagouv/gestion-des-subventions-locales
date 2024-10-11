a74b3085d6eb20bd1fdaadc960bd4c901ba29eb2
Agnès Haasser <agnes@hashbang.coop>
2024-10-11 16:19:20 +0200

    Add a command to call my task

---
+++ gsl_demarches_simplifiees/management/commands/ds_save_demarche.py
@@ -0,0 +1,13 @@
+from django.core.management.base import BaseCommand
+
+from gsl_demarches_simplifiees.tasks import save_demarche_from_ds
+
+
+class Command(BaseCommand):
+    help = "Get info about one demarche from its number"
+
+    def add_arguments(self, parser):
+        parser.add_argument("demarche_number", type=int)
+
+    def handle(self, *args, **options):
+        save_demarche_from_ds(options["demarche_number"])
