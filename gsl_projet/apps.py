fd86fc148c62e0b3979972d2cc3a666a264dd481
Agnès Haasser <agnes@hashbang.coop>
2024-11-06 15:42:37 +0100

    Improve readability

--- gsl_projet/apps.py
+++ gsl_projet/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class GslProjetConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "gsl_projet"
+
+    verbose_name = "3. Projets"
