78239ccaf2aa7d3d698394a85343bd4379431517
Agnès Haasser <agnes@hashbang.coop>
2024-11-07 11:37:43 +0100

    Allow filter projets on user

--- gsl_projet/models.py
+++ gsl_projet/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from gsl_core.models import Adresse, Arrondissement, Departement
+from gsl_core.models import Adresse, Arrondissement, Collegue, Departement
 from gsl_demarches_simplifiees.models import Dossier
 
 
@@ -16,6 +16,13 @@ class Demandeur(models.Model):
         return f"Demandeur {self.name}"
 
 
+class ProjetManager(models.Manager):
+    def for_user(self, user: Collegue):
+        return self.filter(
+            dossier_ds__ds_demarche__ds_instructeurs__ds_email=user.email
+        )
+
+
 class Projet(models.Model):
     demandeur = models.ForeignKey(Demandeur, on_delete=models.PROTECT)
     dossier_ds = models.OneToOneField(Dossier, on_delete=models.PROTECT)
@@ -23,6 +30,8 @@ class Projet(models.Model):
     address = models.OneToOneField(Adresse, on_delete=models.CASCADE)
     arrondissement = models.ForeignKey(Arrondissement, on_delete=models.PROTECT)
     departement = models.ForeignKey(Departement, on_delete=models.PROTECT)
+
+    objects = ProjetManager()
 
     def __str__(self):
         return f"Projet {self.pk}"
