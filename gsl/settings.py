3605fb97bf38032b613e9f2016ab93049e8e9c56
Agnès Haasser <agnes@hashbang.coop>
2024-11-07 16:47:35 +0100

    Remove storage entry in settings

--- gsl/settings.py
+++ gsl/settings.py
@@ -173,12 +173,6 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-STORAGES = {
-    # ...
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
