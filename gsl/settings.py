a4cfae2b7b85b70e66d87e01c783f41415856454
Agnès Haasser <agnes@hashbang.coop>
2024-11-15 13:31:06 +0100

    Adjust Celery config to display results in DB

--- gsl/settings.py
+++ gsl/settings.py
@@ -216,4 +216,9 @@ ALLOW_LOGOUT_GET_METHOD = True
 # Celery configuration
 
 CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL", "redis://localhost:6379")
-CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND", "redis://localhost:6379")
+CELERY_ACCEPT_CONTENT = {"application/json"}
+CELERY_RESULT_SERIALIZER = "json"
+CELERY_TASK_SERIALIZER = "json"
+CELERY_TIMEZONE = "Europe/Paris"
+CELERY_RESULT_BACKEND = "django-db"
+CELERY_RESULT_EXTENDED = True
