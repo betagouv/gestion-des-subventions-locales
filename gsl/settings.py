3b990dc83a7ee9a68b7f62c50d01e5e6560e6832
Agnès Haasser <agnes@hashbang.coop>
2024-12-04 13:58:37 +0100

    Install unfold admin interface

--- gsl/settings.py
+++ gsl/settings.py
@@ -57,6 +57,11 @@ if SENTRY_DSN:
 # Application definition
 
 INSTALLED_APPS = [
+    "unfold",  # before django.contrib.admin
+    # "unfold.contrib.filters",  # optional, if special filters are needed
+    # "unfold.contrib.forms",  # optional, if special form elements are needed
+    # "unfold.contrib.inlines",  # optional, if special inlines are needed
+    # "unfold.contrib.import_export",  # optional, if django-import-export package is used
     "django.contrib.admin",
     "django.contrib.auth",
     "mozilla_django_oidc",
--- gsl_projet/admin.py
+++ gsl_projet/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin
 
 from gsl_core.admin import AllPermsForStaffUser
 
@@ -6,12 +7,12 @@ from .models import Demandeur, Projet
 
 
 @admin.register(Demandeur)
-class DemandeurAdmin(AllPermsForStaffUser, admin.ModelAdmin):
+class DemandeurAdmin(AllPermsForStaffUser, ModelAdmin):
     pass
 
 
 @admin.register(Projet)
-class ProjetAdmin(AllPermsForStaffUser, admin.ModelAdmin):
+class ProjetAdmin(AllPermsForStaffUser, ModelAdmin):
     raw_id_fields = ("address", "departement")
     list_display = ("__str__", "dossier_ds__ds_state", "address", "departement")
     list_filter = ("departement", "dossier_ds__ds_state")
