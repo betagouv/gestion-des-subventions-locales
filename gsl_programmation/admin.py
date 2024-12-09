70725e30b418581809a36cc15f37856d57dde44f
Agnès Haasser <agnes@hashbang.coop>
2024-12-09 23:42:42 +0100

    Programmation : admin interfaces

--- gsl_programmation/admin.py
+++ gsl_programmation/admin.py
@@ -1,10 +1,19 @@
-# Register your models here.
 from django.contrib import admin
-from unfold.admin import ModelAdmin
+from unfold.admin import ModelAdmin, TabularInline
 
-from .models import Enveloppe
+from .models import Enveloppe, Scenario, SimulationProjet
 
 
 @admin.register(Enveloppe)
 class EnveloppeAdmin(ModelAdmin):
     pass
+
+
+class SimulationProjetInline(TabularInline):
+    model = SimulationProjet
+    fields = ("projet", "enveloppe", "montant", "taux", "status")
+
+
+@admin.register(Scenario)
+class ScenarioAdmin(ModelAdmin):
+    inlines = (SimulationProjetInline,)
