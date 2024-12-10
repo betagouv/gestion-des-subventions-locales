e505f51d49d929d185f6d3402343a2b9f86fe4cd
Agnès Haasser <agnes@hashbang.coop>
2024-12-10 16:59:51 +0100

    Integration projets in simulation

--- gsl_programmation/admin.py
+++ gsl_programmation/admin.py
@@ -17,3 +17,9 @@ class SimulationProjetInline(TabularInline):
 @admin.register(Scenario)
 class ScenarioAdmin(ModelAdmin):
     pass
+
+
+@admin.register(SimulationProjet)
+class SimulationProjetAdmin(ModelAdmin):
+    list_display = ("__str__", "projet__dossier_ds__projet_intitule")
+    search_fields = ("projet__dossier_ds__projet_intitule",)
