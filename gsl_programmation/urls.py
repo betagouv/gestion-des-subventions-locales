828ce740b6f29a058e4055224d1c662bb45306d3
Agnès Haasser <agnes@hashbang.coop>
2024-12-10 10:40:46 +0100

    Add breadcrumb on scenario detail view

--- gsl_programmation/urls.py
+++ gsl_programmation/urls.py
@@ -4,8 +4,13 @@ from . import views
 
 urlpatterns = [
     path(
+        "simulations/",
+        views.ScenarioListView.as_view(),
+        name="scenario_list",
+    ),
+    path(
         "simulation/<slug:slug>/",
         views.ScenarioDetailView.as_view(),
         name="scenario_detail",
-    )
+    ),
 ]
--- gsl_programmation/views.py
+++ gsl_programmation/views.py
@@ -1,14 +1,40 @@
+from django.urls import reverse
 from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 
 from .models import Scenario, SimulationProjet
+
+
+class ScenarioListView(ListView):
+    model = Scenario
+    paginate_by = 10
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = (
+            "Mes simulations"  # todo si filtre par année : rappeler l'année ici
+        )
+        return context
 
 
 class ScenarioDetailView(DetailView):
     model = Scenario
 
     def get_context_data(self, **kwargs):
+        scenario = self.get_object()
         context = super().get_context_data(**kwargs)
-        context["simulations"] = SimulationProjet.objects.filter(
-            scenario=self.get_object()
+        context["simulations"] = SimulationProjet.objects.filter(scenario=scenario)
+        context["title"] = (
+            f"{scenario.enveloppe.type} {scenario.enveloppe.annee} – {scenario.title}"
         )
+
+        context["breadcrumb_dict"] = {
+            "links": [
+                {
+                    "url": reverse("programmation:scenario_list"),
+                    "title": "Mes simulations de programmation",
+                }
+            ],
+            "current": scenario.title,
+        }
         return context
