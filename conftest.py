4398be5fc131adfeb988a11350c9409df360c7c1
Agnès Haasser <agnes@hashbang.coop>
2024-12-18 15:06:54 +0100

    Pass celery on eager mode during debug and tests

--- conftest.py
+++ conftest.py
@@ -1,8 +1,1 @@
-import pytest
-
 pytest_plugins = ("celery.contrib.pytest",)
-
-
-@pytest.fixture(scope="session")
-def celery_config():
-    return {"broker_url": "redis://", "result_backend": "redis://"}
