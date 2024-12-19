c6390cad93a5405b8974f102f98b81a81cf4d941
Agnès Haasser <agnes@hashbang.coop>
2024-12-19 10:11:05 +0100

    Put celery on eager mode only in tests

--- conftest.py
+++ conftest.py
@@ -1,1 +1,9 @@
+import pytest
+
 pytest_plugins = ("celery.contrib.pytest",)
+
+
+@pytest.fixture(autouse=True)
+def celery_eager_mode(settings):
+    settings.CELERY_TASK_ALWAYS_EAGER = True
+    settings.CELERY_TASK_EAGER_PROPAGATES = True
