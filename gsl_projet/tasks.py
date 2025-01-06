2d541a7e173bb254402b6593dd11011becaf7e15
Agnès Haasser <agnes@hashbang.coop>
2025-01-06 14:24:34 +0100

    Create cron task to update every projet from dossier

--- gsl_projet/tasks.py
+++ gsl_projet/tasks.py
@@ -9,3 +9,12 @@ from .models import Projet
 def update_projet_from_dossier(ds_dossier_number):
     ds_dossier = Dossier.objects.get(ds_number=ds_dossier_number)
     Projet.get_or_create_from_ds_dossier(ds_dossier)
+
+
+@shared_task
+def create_all_projets_from_dossiers():
+    dossiers = Dossier.objects.exclude(ds_state=Dossier.STATE_EN_CONSTRUCTION).exclude(
+        ds_state=""
+    )
+    for dossier in dossiers.all():
+        update_projet_from_dossier.delay(dossier.ds_number)
