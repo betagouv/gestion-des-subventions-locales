88b4683bdf40b1a937dfcc5b44db05677f04286f
maximallain <maxime.allain.8@gmail.com>
2025-01-07 09:48:33 +0100

    refacto: extract filter logic into a mixin

--- gsl_projet/views.py
+++ gsl_projet/views.py
@@ -77,7 +77,57 @@ def get_projet(request, projet_id):
     return render(request, "gsl_projet/projet.html", context)
 
 
-class ProjetListView(ListView):
+class FilterProjectsMixin:
+    def get_filtered_projets(self):
+        qs = Projet.objects.for_user(self.request.user).filter(
+            dossier_ds__ds_date_depot__gte=datetime.date(2024, 9, 1)
+        )
+        filters = self.request.GET
+
+        dispositif = filters.get("dispositif")
+        if dispositif:
+            qs = qs.filter(dossier_ds__demande_dispositif_sollicite=dispositif)
+
+        cout_min = filters.get("cout_min")
+        if cout_min and cout_min.isnumeric():
+            # qs = qs.filter(dossier_ds__finance_cout_total__gte=cout_min)
+            qs = qs.filter(
+                Q(assiette__isnull=False, assiette__gte=cout_min)
+                | Q(assiette__isnull=True, dossier_ds__finance_cout_total__gte=cout_min)
+            )
+
+        cout_max = filters.get("cout_max")
+        if cout_max and cout_max.isnumeric():
+            qs = qs.filter(
+                Q(assiette__isnull=False, assiette__lte=cout_max)
+                | Q(assiette__isnull=True, dossier_ds__finance_cout_total__lte=cout_max)
+            )
+
+        porteur = filters.get("porteur")
+        if porteur in self.PORTEUR_MAPPINGS:
+            qs = qs.filter(
+                dossier_ds__porteur_de_projet_nature__label__in=self.PORTEUR_MAPPINGS.get(
+                    porteur
+                )
+            )
+
+        return qs
+
+    def get_ordering(self):
+        ordering_map = {
+            "date_desc": "-dossier_ds__ds_date_depot",
+            "date_asc": "dossier_ds__ds_date_depot",
+            "cout_desc": "-dossier_ds__finance_cout_total",
+            "cout_asc": "dossier_ds__finance_cout_total",
+            "commune_desc": "-address__commune__name",
+            "commune_asc": "address__commune__name",
+        }
+
+        ordering = self.request.GET.get("tri")
+        return ordering_map.get(ordering, None)
+
+
+class ProjetListView(FilterProjectsMixin, ListView):
     model = Projet
     paginate_by = 25
     PORTEUR_MAPPINGS = {
@@ -91,53 +141,8 @@ class ProjetListView(ListView):
         context["porteur_mappings"] = self.PORTEUR_MAPPINGS
         return context
 
-    def get_ordering(self):
-        ordering_map = {
-            "date_desc": "-dossier_ds__ds_date_depot",
-            "date_asc": "dossier_ds__ds_date_depot",
-            "cout_desc": "-dossier_ds__finance_cout_total",
-            "cout_asc": "dossier_ds__finance_cout_total",
-            "commune_desc": "-address__commune__name",
-            "commune_asc": "address__commune__name",
-        }
-
-        tri = self.request.GET.get("tri")
-        return ordering_map.get(tri, None)
-
     def get_queryset(self):
-        qs = Projet.objects.for_user(self.request.user).filter(
-            dossier_ds__ds_date_depot__gte=datetime.date(2024, 9, 1)
-        )
-
-        # Filtre par dispositif
-        dispositif = self.request.GET.get("dispositif")
-        if dispositif:
-            qs = qs.filter(dossier_ds__demande_dispositif_sollicite=dispositif)
-
-        # Filtre par coût total minimum
-        cout_min = self.request.GET.get("cout_min")
-        if cout_min and cout_min.isnumeric():
-            # qs = qs.filter(dossier_ds__finance_cout_total__gte=cout_min)
-            qs = qs.filter(
-                Q(assiette__isnull=False, assiette__gte=cout_min)
-                | Q(assiette__isnull=True, dossier_ds__finance_cout_total__gte=cout_min)
-            )
-
-        # Filtre par coût total maximum
-        cout_max = self.request.GET.get("cout_max")
-        if cout_max and cout_max.isnumeric():
-            qs = qs.filter(
-                Q(assiette__isnull=False, assiette__lte=cout_max)
-                | Q(assiette__isnull=True, dossier_ds__finance_cout_total__lte=cout_max)
-            )
-
-        porteur = self.request.GET.get("porteur")
-        if porteur in self.PORTEUR_MAPPINGS:
-            qs = qs.filter(
-                dossier_ds__porteur_de_projet_nature__label__in=self.PORTEUR_MAPPINGS.get(
-                    porteur
-                )
-            )
+        qs = self.get_filtered_projets()
 
         # Tri
         ordering = self.get_ordering()
