d65ba8ad0f9d4ee305926c7d91b8c475839ec9fb
maximallain <maxime.allain.8@gmail.com>
2025-01-14 18:11:50 +0100

    feat(simulation_detail): apply ordering

--- gsl_programmation/views.py
+++ gsl_programmation/views.py
@@ -70,6 +70,7 @@ class SimulationDetailView(DetailView, FilterProjetsMixin):
         simulation = self.get_object()
         qs = Projet.objects.filter(simulationprojet__simulation=simulation)
         qs = self.add_filters_to_projets_qs(qs)
+        qs = self.add_ordering_to_projets_qs(qs)
         qs = qs.select_related("address").select_related("address__commune")
         qs = qs.prefetch_related(
             Prefetch(
