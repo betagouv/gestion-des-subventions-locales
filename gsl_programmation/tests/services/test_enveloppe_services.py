0b64b8d48d44538d9afb2d88e18c434a7a161edf
maximallain <maxime.allain.8@gmail.com>
2025-01-17 10:22:01 +0100

    feat: add get_total_amount_asked enveloppe info

--- gsl_programmation/tests/services/test_enveloppe_services.py
+++ gsl_programmation/tests/services/test_enveloppe_services.py
@@ -1,5 +1,6 @@
 import pytest
 
+from gsl_demarches_simplifiees.tests.factories import DossierFactory
 from gsl_programmation.models import SimulationProjet
 from gsl_programmation.services import EnveloppeService
 from gsl_programmation.tests.factories import (
@@ -7,6 +8,7 @@ from gsl_programmation.tests.factories import (
     SimulationFactory,
     SimulationProjetFactory,
 )
+from gsl_projet.tests.factories import ProjetFactory
 
 
 @pytest.fixture
@@ -21,18 +23,27 @@ def simulation(enveloppe):
 
 @pytest.fixture
 def simulation_projet1(enveloppe, simulation):
+    projet = ProjetFactory.create(
+        assiette=1000.00,
+    )
     return SimulationProjetFactory.create(
-        enveloppe=enveloppe, simulation=simulation, montant=100.00
+        enveloppe=enveloppe, simulation=simulation, montant=100.00, projet=projet
     )
 
 
 @pytest.fixture
 def simulation_projet2(enveloppe, simulation):
+    projet = ProjetFactory.create(
+        dossier_ds=DossierFactory.create(
+            finance_cout_total=2000.00,
+        ),
+    )
     return SimulationProjetFactory.create(
         enveloppe=enveloppe,
         simulation=simulation,
         montant=200.00,
         status=SimulationProjet.STATUS_VALID,
+        projet=projet,
     )
 
 
@@ -43,3 +54,12 @@ def test_get_total_amount_validated(enveloppe, simulation_projet1, simulation_pr
 
     # Assert the expected result
     assert total_amount == 200.00
+
+
+@pytest.mark.django_db
+def test_get_total_amount_asked(enveloppe, simulation_projet1, simulation_projet2):
+    # Call the method
+    total_amount = EnveloppeService.get_total_amount_asked(enveloppe)
+
+    # Assert the expected result
+    assert total_amount == 3000.00
