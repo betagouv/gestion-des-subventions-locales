1c5c716c2d8b653166cf7d395824c20380712078
Agnès Haasser <agnes@hashbang.coop>
2025-01-22 16:10:25 +0100

    Import of DETR enveloppes

--- gsl_programmation/admin.py
+++ gsl_programmation/admin.py
@@ -1,20 +1,24 @@
 from django.contrib import admin
+from import_export.admin import ImportExportMixin
+
+from gsl_core.admin import AllPermsForStaffUser
 
 from .models import Enveloppe, Simulation, SimulationProjet
+from .resources import EnveloppeDETRResource, EnveloppeDSILResource
 
 
 @admin.register(Enveloppe)
-class EnveloppeAdmin(admin.ModelAdmin):
-    pass
+class EnveloppeAdmin(AllPermsForStaffUser, ImportExportMixin, admin.ModelAdmin):
+    resource_classes = (EnveloppeDETRResource, EnveloppeDSILResource)
 
 
 @admin.register(Simulation)
-class SimulationAdmin(admin.ModelAdmin):
+class SimulationAdmin(AllPermsForStaffUser, admin.ModelAdmin):
     pass
 
 
 @admin.register(SimulationProjet)
-class SimulationProjetAdmin(admin.ModelAdmin):
+class SimulationProjetAdmin(AllPermsForStaffUser, admin.ModelAdmin):
     list_display = ("__str__", "projet__dossier_ds__projet_intitule")
     search_fields = ("projet__dossier_ds__projet_intitule",)
     raw_id_fields = ("projet",)
