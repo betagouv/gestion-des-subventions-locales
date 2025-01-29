8268e3e6da41bf1f91fa6ca0ce24cdbae19f970b
Agnès Haasser <agnes@hashbang.coop>
2025-01-29 14:51:48 +0100

    Remove unused code in Form

--- gsl_programmation/forms.py
+++ gsl_programmation/forms.py
@@ -3,7 +3,6 @@ from django.core.exceptions import ValidationError
 from dsfr.forms import DsfrBaseForm
 
 from gsl_core.models import Perimetre
-from gsl_programmation.models import Enveloppe
 
 
 class SimulationForm(DsfrBaseForm):
@@ -28,35 +27,14 @@ class SimulationForm(DsfrBaseForm):
         cleaned_data = super().clean()
         dotation = cleaned_data.get("dotation")
         if self.user.perimetre is None:
-            raise ValidationError("Vous n'avez pas de perimetre. Contactez l'équipe.")
+            raise ValidationError(
+                "Votre compte n’est pas associé à un périmètre. Contactez l’équipe."
+            )
 
         if dotation == "DETR":
             if self.user.perimetre.type == Perimetre.TYPE_REGION:
                 raise ValidationError(
-                    f"Vous n'avez pas de département associé à votre périmètre ({self.user.perimetre}). Contactez l'équipe."
+                    f"Votre compte n’est pas associé à un périmètre départemental ({self.user.perimetre}). Contactez l’équipe."
                 )
-
-            enveloppe_perimetre = Perimetre.objects.get(
-                region=self.user.perimetre.region,
-                departement=self.user.perimetre.departement,
-                arrondissement=None,
-            )
-
-        if dotation == "DSIL":
-            enveloppe_perimetre = Perimetre.objects.get(
-                region=self.user.perimetre.region,
-                departement=None,
-                arrondissement=None,
-            )
-
-        if (
-            Enveloppe.objects.filter(
-                perimetre=enveloppe_perimetre, type=dotation
-            ).count()
-            == 0
-        ):
-            raise ValidationError(
-                f"Aucun fonds de dotation {dotation} n'a été trouvé pour votre périmètre ({self.user.perimetre}). Contactez l'équipe."
-            )
 
         return cleaned_data
