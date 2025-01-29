0ec62bde801008647d14865d3ed2764bf4735386
maximallain <maxime.allain.8@gmail.com>
2025-01-29 14:39:51 +0100

    fix: display user name containing first and last names

--- gsl_core/tests/test_user_model.py
+++ gsl_core/tests/test_user_model.py
@@ -1,6 +1,7 @@
 import pytest
 
 from gsl_core.models import Collegue
+from gsl_core.tests.factories import CollegueFactory
 
 
 @pytest.mark.django_db
@@ -9,3 +10,14 @@ def test_create_collegue():
     c = Collegue.objects.create(username="hubert.lingot@example.net")
     assert Collegue.objects.count() == 1
     assert c.username == "hubert.lingot@example.net"
+
+
+def test_collegue__str__():
+    only_first_name = CollegueFactory.build(first_name="Bernard", username="Azerty")
+    assert only_first_name.__str__() == "Bernard"
+
+    only_last_name = CollegueFactory.build(last_name="Morin", username="Azerty")
+    assert only_last_name.__str__() == "Morin"
+
+    only_first_name = CollegueFactory.build(first_name="Bernard", last_name="Morin")
+    assert only_first_name.__str__() == "Bernard Morin"
