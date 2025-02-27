a7dc619e30c97a90837ecd987ea54bc2fa0c2078
maximallain <maxime.allain.8@gmail.com>
2025-02-27 17:37:03 +0100

    feat(projet): add refuse confirmation modale

--- gsl_simulation/static/js/simulationProjetAcceptConfirmation.js
+++ gsl_simulation/static/js/simulationProjetAcceptConfirmation.js
@@ -1,32 +1,42 @@
 let selectedElement = null;
 
 function handleStatusChange(select) {
-    if (select.value === "valid") {
-        showConfirmationModal(select);
+    if (["valid", "cancelled"].includes(select.value)) {
+        showConfirmationModal(select, select.value);
     } else {
         htmx.trigger(select.form, 'status-confirmed');  // Déclenche le PATCH HTMX
     }
 }
 
-function showConfirmationModal(select) {
+function showConfirmationModal(select, status) {
+    switch (status) {
+        case "valid":
+            modalId = 'accept-confirmation-modal';
+            break;
+        case "cancelled":
+            modalId = 'refuse-confirmation-modal';
+            break;
+    }
     selectedElement = select;
-    modal = document.getElementById('accept-confimation-modal')
+    modal = document.getElementById(modalId)
     dsfr(modal).modal.disclose()
 }
 
-function closeModal() {
-    modal = document.getElementById('accept-confimation-modal')
+function closeModal(modalId) {
+    modal = document.getElementById(modalId)
     selectedElement.form.reset()
     dsfr(modal).modal.conceal()
     selectedElement.focus()
     selectedElement = null;
 }
 
-document.getElementById('confirmChange').addEventListener('click', function () {
-    if (selectedElement) {
-        selectedElement.form.submit();
-    }
-    else {
-        closeModal();
-    }
+document.querySelectorAll('#confirmChange').forEach((e) => {
+    e.addEventListener('click', function () {
+        if (selectedElement) {
+            selectedElement.form.submit();
+        }
+        else {
+            closeModal();
+        }
+    })
 });
