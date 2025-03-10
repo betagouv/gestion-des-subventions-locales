db41a21a5eb8c9fca1651f53407a2137c67cdb71
maxi_malain <32595427+maximallain@users.noreply.github.com>
2025-03-10 11:46:47 +0100

    chore: use enumerate
    Co-authored-by: Agnès Haasser <1035145+tut-tuuut@users.noreply.github.com>

--- gsl_demarches_simplifiees/importer/dossier.py
+++ gsl_demarches_simplifiees/importer/dossier.py
@@ -15,8 +15,7 @@ def camelcase(my_string):
 def save_demarche_dossiers_from_ds(demarche_number):
     client = DsClient()
     demarche_dossiers = client.get_demarche_dossiers(demarche_number)
-    i = 1
-    for dossier_data in demarche_dossiers:
+    for i, dossier_data in enumerate(demarche_dossiers):
         try:
             ds_id = dossier_data["id"]
             ds_dossier_number = dossier_data["number"]
@@ -25,7 +24,6 @@ def save_demarche_dossiers_from_ds(demarche_number):
             )
             dossier.save()
             refresh_dossier_from_saved_data.delay(dossier.ds_number)
-            i += 1
         except Exception as e:
             logging.error(
                 f"Erreur pour le {i}ème dossier de la démarche {demarche_number}",
