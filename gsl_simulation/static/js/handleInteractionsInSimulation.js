c4e3cf222a633a03254958e184f68ad975aef89c
Agnès Haasser <agnes@hashbang.coop>
2025-03-12 11:30:17 +0100

    Handle enter + change in simulation fields

--- gsl_simulation/static/js/handleInteractionsInSimulation.js
+++ gsl_simulation/static/js/handleInteractionsInSimulation.js
@@ -1,5 +1,19 @@
-console.log("hoho")
-document.querySelector(".gsl-projet-table").addEventListener("keyup", (ev) => {
+'use strict'
 
-})
+if (htmx !== undefined) {
+    document.querySelector(".gsl-projet-table").addEventListener("keyup", (ev) => {
+        if (ev.key !== "Enter") {
+            return;
+        }
+
+        if (ev.target.hasAttribute("hx-post") && !ev.target.disabled) {
+            htmx.trigger(ev.target, 'change');
+            ev.preventDefault();
+        }
+    })
+
+    document.querySelector(".gsl-projet-table").addEventListener("submit", (ev) => {
+        ev.preventDefault()
+    })
+}
 
