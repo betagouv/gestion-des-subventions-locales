a15fef564781351d48087210b63ad38094b8b6be
maximallain <maxime.allain.8@gmail.com>
2025-03-21 10:31:59 +0100

    refactor: simplify js

--- gsl_simulation/static/js/handleInteractionInSimulationProjet.js
+++ gsl_simulation/static/js/handleInteractionInSimulationProjet.js
@@ -9,23 +9,10 @@ document.querySelector("#avis_commission_detr").addEventListener("change", (ev) 
   if(ev.target) ev.target.form.submit();
 })
 
-document.querySelector("#is-in-qpv-and-is-attached-to-a-crte-form").addEventListener("change", (ev) => {
-  disableIsInQPVAndIsAttachedToACrteSubmitButtonIfValuesHasChanged()
-})
-
-function disableIsInQPVAndIsAttachedToACrteSubmitButtonIfValuesHasChanged(){
-  const button=document.querySelector("#submit-is-in-qpv-and-is-attached-to-a-crte");
-
-  original_is_in_qpv = button.dataset.isInQpv == "True"
-  original_is_attached_to_a_crte = button.dataset.isAttachedToACrte == "True"
+document.querySelectorAll(".form-disabled-before-value-change").forEach((elt) => {
+  let button = elt.querySelector("button[type='submit']")
   
-  current_is_in_qpv = document.querySelector("#is-in-qpv").checked
-  current_is_attached_to_a_crte = document.querySelector("#is-attached-to-a-crte").checked
-
-  if(current_is_in_qpv !== original_is_in_qpv || current_is_attached_to_a_crte !== original_is_attached_to_a_crte){
+  elt.addEventListener("change", (ev) => {
     button.disabled = false;
-  } else {
-    button.disabled = true;
-  }
-}
-disableIsInQPVAndIsAttachedToACrteSubmitButtonIfValuesHasChanged()
+  })
+})
