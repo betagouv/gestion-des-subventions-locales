3f896939a994be0cad013d6ff4aff16b4d442223
maximallain <maxime.allain.8@gmail.com>
2025-03-25 17:44:56 +0100

    fix: kill LoginRequiredUserPassesTestMixin

--- gsl_simulation/views/mixins.py
+++ gsl_simulation/views/mixins.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.mixins import UserPassesTestMixin
-from django.contrib.auth.views import redirect_to_login
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
@@ -7,33 +6,8 @@ from django.urls import reverse_lazy
 from gsl_programmation.services.enveloppe_service import EnveloppeService
 from gsl_simulation.models import SimulationProjet
 
-"""
-UserPassesTestMixin cannot be stacked
-https://docs.djangoproject.com/en/4.0/topics/auth/default/#django.contrib.auth.mixins.UserPassesTestMixin.get_test_func
-https://stackoverflow.com/a/60302594/4293684
 
-How to have LoginRequiredMixin (redirects to next url if anonymous) + custom mixin ?
---> custom dispatch() method
-"""
-
-
-class LoginRequiredUserPassesTestMixin(UserPassesTestMixin):
-    """
-    Custom mixin that mimicks the LoginRequiredMixin behavior
-    https://stackoverflow.com/a/59661739
-    """
-
-    def dispatch(self, request, *args, **kwargs):
-        if not request.user.is_authenticated:
-            return redirect_to_login(
-                request.get_full_path(),
-                self.get_login_url(),
-                self.get_redirect_field_name(),
-            )
-        return super().dispatch(request, *args, **kwargs)
-
-
-class CorrectUserPerimeterRequiredMixin(LoginRequiredUserPassesTestMixin):
+class CorrectUserPerimeterRequiredMixin(UserPassesTestMixin):
     def test_func(self):
         user = self.request.user
         simulation_projet = get_object_or_404(
