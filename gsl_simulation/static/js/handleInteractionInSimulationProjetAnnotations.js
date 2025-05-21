51ac87d9a9d20fd90ddc3a82daf229185ac15299
maximallain <maxime.allain.8@gmail.com>
2025-05-21 13:13:31 +0200

    tests: url, form avec view

--- gsl_simulation/static/js/handleInteractionInSimulationProjetAnnotations.js
+++ gsl_simulation/static/js/handleInteractionInSimulationProjetAnnotations.js
@@ -1,3 +1,4 @@
+let isFormDirty = false;
 
 document.addEventListener('DOMContentLoaded', function () {
   const form = document.querySelector("#projet_note_form");
@@ -19,16 +20,32 @@ document.addEventListener('DOMContentLoaded', function () {
   // Vérifie si Maj (Shift) + Entrée sont pressés
     if (event.key === "Enter" && (event.shiftKey || event.metaKey)) {
       event.preventDefault(); // empêche d'ajouter un saut de ligne
+      isFormDirty = false;
       form.submit();          // soumet le formulaire
     }
   }
 
-  const input = form.querySelector("input");
-  const textarea = form.querySelector("textarea");
   form.addEventListener("keydown", function (event) {
     submitForm(event);
   });
-  // textarea.addEventListener("keydown", function (event) {
-  //   submitForm(event);
-  // });
+
+
+  if (!form) return;
+
+  form.addEventListener('input', function () {
+    isFormDirty = true;
+  });
+
+  // Optionnel : désactiver l'avertissement si le formulaire est soumis
+  form.addEventListener('submit', function () {
+    isFormDirty = false;
+  });
+});
+
+    
+// Avant de quitter ou rafraîchir la page
+window.addEventListener('beforeunload', function (e) {
+  if (isFormDirty) {
+    e.preventDefault(); // Nécessaire pour certains navigateurs
+  }
 })
