5e6bd6fb02d6e74afa170ac28c639fa8fc46e9e4
maximallain <maxime.allain.8@gmail.com>
2025-06-18 17:38:48 +0200

    chore: add admin page

--- gsl_notification/apps.py
+++ gsl_notification/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class GslNotificationConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "gsl_notification"
+
+    verbose_name = "6. Notification"
