5b6949ea98f11caa9ad9752b4a41c75bf20aa1da
maximallain <maxime.allain.8@gmail.com>
2025-06-18 11:56:45 +0200

    feat: add save button

--- gsl_notification/views.py
+++ gsl_notification/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse
 
 from gsl_notification.forms import ArreteSigneForm
 from gsl_notification.models import ArreteSigne
+from gsl_notification.utils import (
+    update_file_name_to_put_it_in_a_programmation_projet_folder,
+)
 from gsl_programmation.models import ProgrammationProjet
 
 
@@ -9,11 +13,12 @@ def create_arrete_view(request, programmation_projet_id):
     programmation_projet = get_object_or_404(
         ProgrammationProjet,
         id=programmation_projet_id,
+        status=ProgrammationProjet.STATUS_ACCEPTED,
     )
     if hasattr(programmation_projet, "arrete_signe"):
-        return redirect(
-            "gsl_notification:get-arrete",
-            programmation_projet_id=programmation_projet.id,
+        source_simulation_projet_id = request.GET.get("source_simulation_projet_id")
+        return _redirect_to_get_arrete_view(
+            request, programmation_projet.id, source_simulation_projet_id
         )
 
     if request.method == "POST":
@@ -21,8 +26,17 @@ def create_arrete_view(request, programmation_projet_id):
         if form.is_valid():
             form.instance.programmation_projet = programmation_projet
             form.instance.created_by = request.user
+            update_file_name_to_put_it_in_a_programmation_projet_folder(
+                form.instance.file, programmation_projet.id
+            )
             form.save()
-            return redirect("gsl_notification:get-arrete", pk=programmation_projet.id)
+
+            source_simulation_projet_id = request.POST.get(
+                "source_simulation_projet_id"
+            )
+            return _redirect_to_get_arrete_view(
+                request, programmation_projet.id, source_simulation_projet_id
+            )
     else:
         form = ArreteSigneForm()
 
@@ -59,6 +73,9 @@ def get_arrete_view(request, programmation_projet_id):
         "programmation_projet": programmation_projet,
         "arrete_signe": arrete_signe,
         "projet": programmation_projet.projet,
+        "source_simulation_projet_id": request.GET.get(
+            "source_simulation_projet_id", None
+        ),
         "stepper_dict": {
             "current_step_id": 1,
             "current_step_title": "1 - Création de l’arrêté",
@@ -67,3 +84,15 @@ def get_arrete_view(request, programmation_projet_id):
         },
     }
     return render(request, "get_arrete.html", context=context)
+
+
+def _redirect_to_get_arrete_view(
+    request, programmation_projet_id, source_simulation_projet_id=None
+):
+    url = reverse(
+        "gsl_notification:get-arrete",
+        kwargs={"programmation_projet_id": programmation_projet_id},
+    )
+    if source_simulation_projet_id:
+        url += f"?source_simulation_projet_id={source_simulation_projet_id}"
+    return redirect(url)
