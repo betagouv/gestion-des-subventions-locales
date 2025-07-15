7fa3c7d9cb99ab963924ad273098e24c54a2acd1
maximallain <maxime.allain.8@gmail.com>
2025-07-15 15:45:43 +0200

    feat: create a ui modal

---
+++ ui/templatetags/ui_tags.py
@@ -0,0 +1,14 @@
+from django import template
+from dsfr.utils import parse_tag_args
+
+register = template.Library()
+
+
+@register.inclusion_tag("ui/components/confirmation_modal.html")
+def ui_confirmation_modal(*args, **kwargs) -> dict:
+    allowed_keys = [
+        "modal_id",
+    ]
+    tag_data = parse_tag_args(args, kwargs, allowed_keys)
+
+    return {"self": tag_data}
