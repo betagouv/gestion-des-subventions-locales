cb47d663acd631dc888a9d231a438f9f7f9cf3bc
maximallain <maxime.allain.8@gmail.com>
2025-07-15 16:34:01 +0200

    refactor: use modal for delete in annotions

--- ui/templatetags/ui_tags.py
+++ ui/templatetags/ui_tags.py
@@ -6,9 +6,7 @@ register = template.Library()
 
 @register.inclusion_tag("ui/components/confirmation_modal.html")
 def ui_confirmation_modal(*args, **kwargs) -> dict:
-    allowed_keys = [
-        "modal_id",
-    ]
+    allowed_keys = ["modal_id", "title", "text", "form"]
     tag_data = parse_tag_args(args, kwargs, allowed_keys)
 
     return {"self": tag_data}
