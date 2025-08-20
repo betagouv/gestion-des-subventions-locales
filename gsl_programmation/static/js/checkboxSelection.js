7255d5f8132023a9e850f668f751800948eccf5a
maximallain <maxime.allain.8@gmail.com>
2025-08-20 09:42:06 +0200

    chore: format gsl_programmation

--- gsl_programmation/static/js/checkboxSelection.js
+++ gsl_programmation/static/js/checkboxSelection.js
@@ -1,50 +1,47 @@
-import { Controller } from "stimulus"
+import { Controller } from 'stimulus'
 
 export class CheckboxSelection extends Controller {
   static values = {
-   allCheckboxesSelected: {type:Boolean, default: false},
+    allCheckboxesSelected: { type: Boolean, default: false }
   }
 
-  static targets = [ "globalButton", "button" ]
+  static targets = ['globalButton', 'button']
 
-  connect(){
+  connect () {
     this.allCheckboxesSelectedValue = this.globalButtonTarget.checked
   }
 
-  toggle(){
+  toggle () {
     const newValue = !this.allCheckboxesSelectedValue
     this.allCheckboxesSelectedValue = newValue
     if (newValue) {
       this._updateAllCheckboxes(true)
-    }
-    else {
+    } else {
       this._updateAllCheckboxes(false)
     }
   }
 
-  updateAllCheckboxesSelected(evt){
+  updateAllCheckboxesSelected (evt) {
     const eltValue = evt.target.checked
-    if (eltValue == false){
+    if (eltValue === false) {
       this.allCheckboxesSelectedValue = false
-    }
-    else {
+    } else {
       if (this.buttonTargets.every(b => b.checked)) {
         this.allCheckboxesSelectedValue = true
       }
     }
-    
   }
 
-  allCheckboxesSelectedValueChanged() {
+  allCheckboxesSelectedValueChanged () {
     this._updateACheckbox(this.globalButtonTarget, this.allCheckboxesSelectedValue)
   }
 
-  _updateAllCheckboxes(value) {
-    this.buttonTargets.forEach(element => this._updateACheckbox(element, value));
+  _updateAllCheckboxes (value) {
+    this.buttonTargets.forEach(element => this._updateACheckbox(element, value))
   }
 
-  _updateACheckbox(elt, value){
-    elt.checked= value
-    elt.setAttribute("data-fr-js-checkbox-input", value)
+  _updateACheckbox (elt, value) {
+    elt.checked = value
+    elt.setAttribute('data-fr-js-checkbox-input', value)
   }
 }
