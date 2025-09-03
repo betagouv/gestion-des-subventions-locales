5c843a8c4e4785c7c287ed57b791d673d38a805e
maximallain <maxime.allain.8@gmail.com>
2025-09-03 15:38:25 +0200

    fix: static dir

--- config/settings/default.py
+++ config/settings/default.py
@@ -20,7 +20,7 @@ from dotenv import load_dotenv
 load_dotenv()  # take environment variables from .env.
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
--- config/settings/dev.py
+++ config/settings/dev.py
@@ -2,5 +2,7 @@ from config.settings.default import *  # noqa
 
 DEBUG = True
 
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "localhost:8000", "127.0.0.1:8000"]
+
 INSTALLED_APPS.append("query_counter")  # noqa: F405
 MIDDLEWARE.append("query_counter.middleware.DjangoQueryCounterMiddleware")  # noqa: F405
