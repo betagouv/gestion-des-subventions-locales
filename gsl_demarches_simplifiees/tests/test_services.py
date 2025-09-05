e48fd20250dc1c2845c7fe6331fb4046e3876259
maximallain <maxime.allain.8@gmail.com>
2025-09-05 16:08:41 +0200

    feat: make updload_boolean_field generic

--- gsl_demarches_simplifiees/tests/test_services.py
+++ gsl_demarches_simplifiees/tests/test_services.py
@@ -1,8 +1,10 @@
+from unittest import mock
 from unittest.mock import patch
 
 import pytest
 
 from gsl_core.tests.factories import CollegueFactory
+from gsl_demarches_simplifiees.models import FieldMappingForComputer
 from gsl_demarches_simplifiees.services import (
     DsService,
     DsServiceException,
@@ -33,7 +35,29 @@ def ds_field():
     return FieldMappingForComputerFactory(ds_field_id=101112)
 
 
-def test_update_ds_is_qpv_success(user, dossier, ds_field):
+@pytest.mark.parametrize(
+    "function, field_name",
+    (
+        ("update_ds_is_qpv", "annotations_is_qpv"),
+        ("update_ds_is_crte", "annotations_is_crte"),
+        (
+            "update_ds_is_budget_vert",
+            "annotations_is_budget_vert",
+        ),
+    ),
+)
+@mock.patch.object(DsService, "_update_boolean_field")
+def test_update_boolean_field_functions_call_generic_function_success(
+    mocker, user, dossier, ds_field, function, field_name
+):
+    ds_service = DsService()
+
+    ds_service_function = getattr(ds_service, function)
+    ds_service_function(dossier, user, "true")
+    mocker.assert_called_once_with(dossier, user, "true", field=field_name)
+
+
+def test_update_boolean_field_success(user, dossier, ds_field):
     ds_service = DsService()
     with (
         patch(
@@ -55,28 +79,47 @@ def test_update_ds_is_qpv_success(user, dossier, ds_field):
                 }
             }
         }
-        result = ds_service.update_ds_is_qpv(dossier, user, "true")
+        result = ds_service._update_boolean_field(
+            dossier, user, "true", field="boolean_field"
+        )
         assert "data" in result
 
 
-def test_update_ds_is_qpv_instructeur_unknown(dossier):
+def test_update_boolean_field_instructeur_unknown(dossier):
     user = CollegueFactory(ds_id="")
     service = DsService()
     with pytest.raises(InstructeurUnknown):
         service.update_ds_is_qpv(dossier, user, "true")
 
 
-def test_update_ds_is_qpv_field_error(user, dossier):
+@pytest.mark.parametrize(
+    "function, field_name",
+    (
+        ("update_ds_is_qpv", "Projet situé en QPV"),
+        ("update_ds_is_crte", "Projet rattaché à un CRTE"),
+        (
+            "update_ds_is_budget_vert",
+            "Projet concourant à la transition écologique au sens budget vert",
+        ),
+    ),
+)
+def test_update_update_boolean_field_field_error(user, dossier, function, field_name):
     ds_service = DsService()
     with patch(
         "gsl_demarches_simplifiees.services.FieldMappingForComputer.objects.get",
-        side_effect=FieldError,
+        side_effect=FieldMappingForComputer.DoesNotExist,
     ):
-        with pytest.raises(FieldError):
-            ds_service.update_ds_is_qpv(dossier, user, "true")
+        with pytest.raises(FieldError) as exc_info:
+            ds_service_function = getattr(ds_service, function)
+            ds_service_function(dossier, user, "true")
+
+    assert (
+        str(exc_info.value)
+        == f'Le champs "{field_name}" n\'existe pas dans la démarche.'
+    )
 
 
-def test_update_ds_is_qpv_rights_error(user, dossier, ds_field):
+def test_update_boolean_field_rights_error(user, dossier, ds_field):
     ds_service = DsService()
     with (
         patch(
@@ -97,10 +140,12 @@ def test_update_ds_is_qpv_rights_error(user, dossier, ds_field):
             }
         }
         with pytest.raises(UserRightsError):
-            ds_service.update_ds_is_qpv(dossier, user, "true")
+            ds_service._update_boolean_field(
+                dossier, user, "true", field="boolean_field"
+            )
 
 
-def test_update_ds_is_qpv_ds_service_exception(user, dossier, ds_field):
+def test_update_boolean_field_ds_service_exception(user, dossier, ds_field):
     ds_service = DsService()
     with (
         patch(
@@ -115,4 +160,6 @@ def test_update_ds_is_qpv_ds_service_exception(user, dossier, ds_field):
             ]
         }
         with pytest.raises(DsServiceException):
-            ds_service.update_ds_is_qpv(dossier, user, "true")
+            ds_service._update_boolean_field(
+                dossier, user, "true", field="boolean_field"
+            )
