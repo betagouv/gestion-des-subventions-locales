590d692bb4c3ef50a1e9f10f995c0fd0716f9807
maximallain <maxime.allain.8@gmail.com>
2025-09-12 09:14:44 +0200

    fix: don't ask useless data

--- gsl_demarches_simplifiees/ds_client.py
+++ gsl_demarches_simplifiees/ds_client.py
@@ -115,7 +115,12 @@ class DsMutator(DsClientBase):
     filename = "ds_mutations.gql"
 
     def dossier_modifier_annotation_checkbox(
-        self, dossier_id: str, instructeur_id: str, field_id: str, value: bool
+        self,
+        dossier_id: str,
+        instructeur_id: str,
+        field_id: str,
+        value: bool,
+        include_annotations=False,
     ):
         variables = {
             "input": {
@@ -124,7 +129,8 @@ class DsMutator(DsClientBase):
                 "dossierId": dossier_id,
                 "instructeurId": instructeur_id,
                 "value": value,
-            }
+            },
+            "includeAnnotations": include_annotations,
         }
         return self.launch_graphql_query(
             "modifierAnnotationCheckbox", variables=variables
