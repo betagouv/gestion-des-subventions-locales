462613c934be05e42abb47756e3cf2b544ef3e4c
Maud Royer <hello@maudroyer.fr>
2025-09-23 12:11:09 +0200

    feat(templates): displaying and styling sub-envelopes in summary table

--- gsl_projet/views.py
+++ gsl_projet/views.py
@@ -172,14 +172,12 @@ class ProjetListView(FilterView, ListView, FilterUtils):
         context["total_amount_granted"] = ProjetService.get_total_amount_granted(
             qs_global
         )
-        context["enveloppes"] = [
-            e
-            for p in (
-                self.request.user.perimetre,
-                *self.request.user.perimetre.children(),
-            )
-            for e in p.enveloppe_set.all()
-        ]
+        context["enveloppes"] = self.request.user.perimetre.enveloppe_set.filter(
+            annee=self.request.user.perimetre.enveloppe_set.order_by("-annee")
+            .values_list("annee", flat=True)
+            .first()
+        ).all()
+        context["enveloppes_with_children"] = True
         self.enrich_context_with_filter_utils(context, self.STATE_MAPPINGS)
 
         return context
