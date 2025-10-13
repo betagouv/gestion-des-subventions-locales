fed6b4d2649739050923d3dcb40baa41e3e35f81
Maud Royer <hello@maudroyer.fr>
2025-10-13 17:22:55 +0200

    refactor(validators): create upload validators for use in forms and models
    Signed-off-by: Maud Royer <hello@maudroyer.fr>

--- gsl_notification/forms.py
+++ gsl_notification/forms.py
@@ -1,12 +1,8 @@
-import os
-
 from django import forms
 from django.db import transaction
-from django.db.models.fields import files
 from django.utils import timezone
 from dsfr.forms import DsfrBaseForm
 
-from gsl.settings import MAX_POST_FILE_SIZE_IN_MO
 from gsl_demarches_simplifiees.ds_client import DsMutator
 from gsl_notification.models import (
     Annexe,
@@ -38,41 +34,13 @@ class LettreNotificationForm(ArreteForm):
         fields = ("content", "created_by", "programmation_projet", "modele")
 
 
-class UploadedDocumentForm(forms.ModelForm, DsfrBaseForm):
-    file = forms.FileField(
-        required=True,
-    )
-
-    class Meta:
-        abstract = True
-
-    def clean_file(self):
-        file = self.cleaned_data["file"]
-        valid_mime_types = ["application/pdf", "image/png", "image/jpeg"]
-        valid_extensions = [".pdf", ".png", ".jpg", ".jpeg"]
-
-        ext = os.path.splitext(file.name)[1].lower()
-        if file.content_type not in valid_mime_types or ext not in valid_extensions:
-            raise forms.ValidationError(
-                "Seuls les fichiers PDF, PNG ou JPEG sont acceptés."
-            )
-
-        max_size_in_mo = MAX_POST_FILE_SIZE_IN_MO
-        max_size = max_size_in_mo * 1024 * 1024
-        if file.size > max_size:
-            raise forms.ValidationError(
-                f"La taille du fichier ne doit pas dépasser {max_size_in_mo} Mo."
-            )
-        return file
-
-
-class ArreteEtLettreSigneForm(UploadedDocumentForm):
+class ArreteEtLettreSigneForm(forms.ModelForm, DsfrBaseForm):
     class Meta:
         model = ArreteEtLettreSignes
         fields = ("file", "created_by", "programmation_projet")
 
 
-class AnnexeForm(UploadedDocumentForm):
+class AnnexeForm(forms.ModelForm, DsfrBaseForm):
     class Meta:
         model = Annexe
         fields = ("file", "created_by", "programmation_projet")
@@ -98,32 +66,6 @@ class ModeleDocumentStepTwoForm(forms.ModelForm, DsfrBaseForm):
     class Meta:
         model = ModeleDocument
         fields = ("logo", "logo_alt_text", "top_right_text")
-
-    def clean_logo(self):
-        file = self.cleaned_data["logo"]
-        valid_mime_types = ["image/png", "image/jpeg"]
-        valid_extensions = [".png", ".jpg", ".jpeg"]
-
-        ext = os.path.splitext(file.name)[1].lower()
-
-        if isinstance(file, files.FieldFile):  # Useful for duplicate
-            if ext not in valid_extensions:
-                raise forms.ValidationError(
-                    "Seuls les fichiers PNG ou JPEG sont acceptés."
-                )
-        else:
-            if file.content_type not in valid_mime_types or ext not in valid_extensions:
-                raise forms.ValidationError(
-                    "Seuls les fichiers PNG ou JPEG sont acceptés."
-                )
-
-        max_size_in_mo = MAX_POST_FILE_SIZE_IN_MO
-        max_size = max_size_in_mo * 1024 * 1024
-        if file.size > max_size:
-            raise forms.ValidationError(
-                f"La taille du fichier ne doit pas dépasser {max_size_in_mo} Mo."
-            )
-        return file
 
 
 class ModeleDocumentStepThreeForm(forms.ModelForm, DsfrBaseForm):
