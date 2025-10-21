4a89c0511253f847b8525a5d60028653ff3a4ed7
maximallain <maxime.allain.8@gmail.com>
2025-10-21 09:11:32 +0200

    feat: update admin, tasks avec some gsl_core and gsl_ds tests

--- gsl_core/tasks.py
+++ gsl_core/tasks.py
@@ -4,35 +4,30 @@ from typing import List
 from celery import shared_task
 
 from gsl_core.models import Collegue
-from gsl_demarches_simplifiees.models import Demarche
+from gsl_demarches_simplifiees.models import Profile
 
 logger = logging.getLogger(__name__)
 
 
 @shared_task
-def associate_or_update_ds_id_to_users(user_ids: List[int]):
+def associate_or_update_ds_profile_to_users(user_ids: List[int]):
     users = Collegue.objects.filter(pk__in=user_ids)
-    logger.info("Association d'id DS avec les utilisateurs : début")
+    logger.info("Association de profil DS avec les utilisateurs : début")
 
     user_without_email_count = users.filter(email="").count()
     if user_without_email_count > 0:
         logger.info(f"{user_without_email_count} utilisateurs n'ont pas d'adresse mail")
 
     user_emails = set(users.exclude(email="").values_list("email", flat=True))
-    demarches = Demarche.objects.all()
+    ds_profiles = Profile.objects.filter(ds_email__in=user_emails)
 
-    for demarche in demarches:
-        instructeurs_groups = demarche.raw_ds_data["groupeInstructeurs"]
-        for instructeurs_group in instructeurs_groups:
-            for instructeur in instructeurs_group["instructeurs"]:
-                instructeur_email = instructeur["email"]
-                if instructeur_email in user_emails:
-                    users.filter(email=instructeur_email).update(
-                        ds_id=instructeur["id"]
-                    )
-                    user_emails.remove(instructeur_email)
+    for user in users:
+        ds_profile = ds_profiles.filter(ds_email=user.email).first()
+        if ds_profile:
+            users.filter(email=user.email).update(ds_profile=ds_profile)
+            user_emails.remove(user.email)
 
-    logger.info("Association d'id DS avec les utilisateurs : fin")
+    logger.info("Association de profil DS avec les utilisateurs : fin")
     if len(user_emails) > 0:
         logger.info(
             f"Ces emails n'ont pas été trouvés dans les groupes instructeurs des démarches : {','.join(user_emails)}"
