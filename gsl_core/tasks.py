de0368d036017366191ebaa15b9f4eba9595b456
maxi_malain <32595427+maximallain@users.noreply.github.com>
2025-10-22 16:06:46 +0200

    Apply suggestions from code review
    fix: enhance perf
    
    Co-authored-by: Maud Royer <pro@maudroyer.fr>

--- gsl_core/tasks.py
+++ gsl_core/tasks.py
@@ -2,6 +2,7 @@ import logging
 from typing import List
 
 from celery import shared_task
+from django.db.models import OuterRef, Subquery
 
 from gsl_core.models import Collegue
 from gsl_demarches_simplifiees.models import Profile
@@ -19,13 +20,12 @@ def associate_or_update_ds_profile_to_users(user_ids: List[int]):
         logger.info(f"{user_without_email_count} utilisateurs n'ont pas d'adresse mail")
 
     user_emails = set(users.exclude(email="").values_list("email", flat=True))
-    ds_profiles = Profile.objects.filter(ds_email__in=user_emails)
 
-    for user in users:
-        ds_profile = ds_profiles.filter(ds_email=user.email).first()
-        if ds_profile:
-            users.filter(email=user.email).update(ds_profile=ds_profile)
-            user_emails.remove(user.email)
+    users.exclude(email=None).update(
+        ds_profile=Subquery(
+            Profile.objects.filter(ds_email=OuterRef("email")).values("pk")[:1]
+        )
+    )
 
     logger.info("Association de profil DS avec les utilisateurs : fin")
     if len(user_emails) > 0:
