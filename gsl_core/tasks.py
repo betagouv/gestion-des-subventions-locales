6d8dc96b3af1c192ce8e2e0cb346089e6511bee9
maximallain <maxime.allain.8@gmail.com>
2025-10-23 17:05:15 +0200

    fix: script

--- gsl_core/tasks.py
+++ gsl_core/tasks.py
@@ -19,16 +19,20 @@ def associate_or_update_ds_profile_to_users(user_ids: List[int]):
     if user_without_email_count > 0:
         logger.info(f"{user_without_email_count} utilisateurs n'ont pas d'adresse mail")
 
-    user_emails = set(users.exclude(email="").values_list("email", flat=True))
-
     users.exclude(email=None).update(
         ds_profile=Subquery(
             Profile.objects.filter(ds_email=OuterRef("email")).values("pk")[:1]
         )
     )
 
+    emails_of_user_without_profile = set(
+        users.exclude(email=None)
+        .filter(ds_profile__isnull=True)
+        .values_list("email", flat=True)
+    )
+
     logger.info("Association de profil DS avec les utilisateurs : fin")
-    if len(user_emails) > 0:
+    if len(emails_of_user_without_profile) > 0:
         logger.info(
-            f"Ces emails n'ont pas été trouvés dans les groupes instructeurs des démarches : {','.join(user_emails)}"
+            f"Ces emails n'ont pas été trouvés dans les groupes instructeurs des démarches : {', '.join(emails_of_user_without_profile)}"
         )
