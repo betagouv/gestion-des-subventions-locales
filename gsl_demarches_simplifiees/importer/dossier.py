426e78db7d4effb880b9446d4fe6230abd433efd
maximallain <maxime.allain.8@gmail.com>
2025-10-24 18:44:14 +0200

    refacto: use always check of ds modif datetime

--- gsl_demarches_simplifiees/importer/dossier.py
+++ gsl_demarches_simplifiees/importer/dossier.py
@@ -11,18 +11,23 @@ from gsl_projet.tasks import create_or_update_projet_and_co_from_dossier
 
 
 def save_demarche_dossiers_from_ds(demarche_number):
-    from gsl_demarches_simplifiees.tasks import task_refresh_dossier_from_saved_data
-
+    demarche = Demarche.objects.get(ds_number=demarche_number)
     client = DsClient()
     demarche_dossiers = client.get_demarche_dossiers(demarche_number)
     for i, dossier_data in enumerate(demarche_dossiers):
         try:
             ds_id = dossier_data["id"]
             ds_dossier_number = dossier_data["number"]
-            dossier = get_or_create_dossier(
-                ds_id, ds_dossier_number, demarche_number, dossier_data
+            dossier, _ = Dossier.objects.get_or_create(
+                ds_id=ds_id,
+                defaults={
+                    "ds_demarche": demarche,
+                    "ds_number": ds_dossier_number,
+                },
             )
-            task_refresh_dossier_from_saved_data.delay(dossier.ds_number)
+            _save_dossier_data_and_refresh_dossier_and_projet_and_co(
+                dossier, dossier_data, async_refresh=True
+            )
         except Exception as e:
             logging.error(
                 f"Erreur pour le {i}ème dossier de la démarche {demarche_number}",
@@ -33,25 +38,50 @@ def save_demarche_dossiers_from_ds(demarche_number):
 def save_one_dossier_from_ds(dossier: Dossier, client: DsClient | None = None):
     client = client or DsClient()
     dossier_data = client.get_one_dossier(dossier.ds_number)
+    has_dossier_been_updated = _save_dossier_data_and_refresh_dossier_and_projet_and_co(
+        dossier, dossier_data
+    )
+
+    if has_dossier_been_updated:
+        return (
+            messages.SUCCESS,
+            "Le dossier a bien été mis à jour depuis Démarches Simplifiées.",
+        )
+    return (
+        messages.WARNING,
+        (
+            "Le dossier était déjà à jour sur Turgot, nous ne l’avons pas "
+            "remis à jour depuis Démarches Simplifiées."
+        ),
+    )
+
+
+def _save_dossier_data_and_refresh_dossier_and_projet_and_co(
+    dossier: Dossier, dossier_data: dict, async_refresh: bool = False
+):
+    has_dossier_been_updated = _has_dossier_been_updated(dossier, dossier_data)
     refresh_dossier_instructeurs(dossier_data, dossier)
+    dossier.raw_ds_data = dossier_data
+    dossier.save()
+
+    if has_dossier_been_updated:
+        if async_refresh:
+            from gsl_demarches_simplifiees.tasks import (
+                task_refresh_dossier_from_saved_data,
+            )
+
+            task_refresh_dossier_from_saved_data.delay(dossier.ds_number)
+        else:
+            refresh_dossier_from_saved_data(dossier)
+
+    return has_dossier_been_updated
+
+
+def _has_dossier_been_updated(dossier: Dossier, dossier_data: dict) -> bool:
     date_modif_ds = dossier_data.get("dateDerniereModification", None)
     if date_modif_ds:
         date_modif_ds = timezone.datetime.fromisoformat(date_modif_ds)
-        if date_modif_ds > dossier.ds_date_derniere_modification:
-            dossier.raw_ds_data = dossier_data
-            refresh_dossier_from_saved_data(dossier)
-            return (
-                messages.SUCCESS,
-                "Le dossier a bien été mis à jour depuis Démarches Simplifiées.",
-            )
-        else:
-            return (
-                messages.WARNING,
-                (
-                    "Le dossier était déjà à jour sur Turgot, nous ne l’avons pas "
-                    "remis à jour depuis Démarches Simplifiées."
-                ),
-            )
+        return date_modif_ds > dossier.ds_date_derniere_modification
 
     raise DsServiceException("Unset date_modif_ds is not a normal situation.")
 
@@ -63,9 +93,7 @@ def refresh_dossier_from_saved_data(dossier: Dossier):
     try:
         dossier.save()
     except Exception as e:
-        logging.error(
-            str(e),
-        )
+        logging.error(str(e))
         raise e
 
     create_or_update_projet_and_co_from_dossier(dossier.ds_number)
@@ -96,19 +124,3 @@ def refresh_dossier_instructeurs(dossier_data, dossier: Dossier):
                 ds_id=instructeur_data["id"], ds_email=instructeur_data["email"]
             )
             dossier.ds_instructeurs.add(instructeur)
-
-
-def get_or_create_dossier(ds_dossier_id, ds_dossier_number, demarche_number, raw_data):
-    dossier_qs = Dossier.objects.filter(ds_id=ds_dossier_id)
-    if dossier_qs.exists():
-        dossier = dossier_qs.get()
-        dossier.raw_ds_data = raw_data
-        dossier.save()
-        return dossier
-    demarche = Demarche.objects.get(ds_number=demarche_number)
-    return Dossier.objects.create(
-        ds_id=ds_dossier_id,
-        ds_demarche=demarche,
-        ds_number=ds_dossier_number,
-        raw_ds_data=raw_data,
-    )
