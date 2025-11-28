07b9c68a42b8f5a5264032e01598ef3f931663bd
maximallain <maxime.allain.8@gmail.com>
2025-11-28 13:48:24 +0100

    feat(projet): create a with_at_least_one_programmated_dotation function in ProjetManager

--- gsl_projet/tests/models/test_projet.py
+++ gsl_projet/tests/models/test_projet.py
@@ -1,7 +1,16 @@
 import pytest
 
+from gsl_core.tests.factories import CollegueFactory, PerimetreDepartementalFactory
 from gsl_programmation.tests.factories import ProgrammationProjetFactory
-from gsl_projet.constants import DOTATION_DETR, DOTATION_DSIL
+from gsl_projet.constants import (
+    DOTATION_DETR,
+    DOTATION_DSIL,
+    PROJET_STATUS_ACCEPTED,
+    PROJET_STATUS_DISMISSED,
+    PROJET_STATUS_PROCESSING,
+    PROJET_STATUS_REFUSED,
+)
+from gsl_projet.models import Projet
 from gsl_projet.tests.factories import DotationProjetFactory, ProjetFactory
 
 pytestmark = pytest.mark.django_db(transaction=True)
@@ -129,3 +138,101 @@ def test_to_notify_with_double_dotation_partial_programmation():
     ProgrammationProjetFactory(dotation_projet=dotation_detr)
 
     assert projet.to_notify is False
+
+
+def test_with_at_least_one_programmated_dotation():
+    """Project with at least one programmated dotation should return True."""
+    projet = ProjetFactory()
+    dotation = DotationProjetFactory(projet=projet, dotation=DOTATION_DETR)
+    ProgrammationProjetFactory(dotation_projet=dotation)
+    assert Projet.objects.with_at_least_one_programmated_dotation().count() == 1
+
+
+def test_with_at_least_one_programmated_dotation_without_programmation():
+    """Project without programmated dotation should return False."""
+    projet = ProjetFactory()
+    DotationProjetFactory(projet=projet, dotation=DOTATION_DETR)
+    assert Projet.objects.with_at_least_one_programmated_dotation().count() == 0
+
+
+def test_with_at_least_one_programmated_dotation_when_projet_has_two_programmated_dotations():
+    """Project without programmated dotation should return False."""
+    projet = ProjetFactory()
+    dotation_detr = DotationProjetFactory(projet=projet, dotation=DOTATION_DETR)
+    dotation_dsil = DotationProjetFactory(projet=projet, dotation=DOTATION_DSIL)
+    ProgrammationProjetFactory(dotation_projet=dotation_detr)
+    ProgrammationProjetFactory(dotation_projet=dotation_dsil)
+    assert Projet.objects.with_at_least_one_programmated_dotation().count() == 1
+
+
+def test_for_user_and_at_least_one_programmated_dotation():
+    perimetre = PerimetreDepartementalFactory()
+    user = CollegueFactory(perimetre=perimetre)
+    projet = ProjetFactory(perimetre=perimetre)
+    dotation_detr = DotationProjetFactory(projet=projet, dotation=DOTATION_DETR)
+    ProgrammationProjetFactory(dotation_projet=dotation_detr)
+
+    projet_not_in_perimeter = ProjetFactory()
+    dotation_detr_not_in_perimeter = DotationProjetFactory(
+        projet=projet_not_in_perimeter, dotation=DOTATION_DETR
+    )
+    ProgrammationProjetFactory(dotation_projet=dotation_detr_not_in_perimeter)
+
+    assert (
+        Projet.objects.for_user(user).with_at_least_one_programmated_dotation().count()
+        == 1
+    )
+
+
+def test_can_display_notification_tab_without_dotations():
+    """Project without any dotations should return False."""
+    projet = ProjetFactory()
+    assert projet.can_display_notification_tab is False
+
+
+def test_can_display_notification_tab_with_accepted_dotation():
+    """Project with processing dotation should return False."""
+    projet = ProjetFactory()
+    DotationProjetFactory(
+        projet=projet, dotation=DOTATION_DETR, status=PROJET_STATUS_ACCEPTED
+    )
+    assert projet.can_display_notification_tab is True
+
+
+@pytest.mark.parametrize(
+    "dotation_status",
+    [PROJET_STATUS_REFUSED, PROJET_STATUS_DISMISSED, PROJET_STATUS_PROCESSING],
+)
+def test_can_display_notification_tab_with_not_accepted_dotation(dotation_status):
+    """Project with not accepted dotation should return False."""
+    projet = ProjetFactory()
+    DotationProjetFactory(projet=projet, dotation=DOTATION_DETR, status=dotation_status)
+    assert projet.can_display_notification_tab is False
+
+
+@pytest.mark.parametrize(
+    "first_dotation_status, second_dotation_status, expected_can_display_notification_tab",
+    [
+        (PROJET_STATUS_ACCEPTED, PROJET_STATUS_ACCEPTED, True),
+        (PROJET_STATUS_ACCEPTED, PROJET_STATUS_REFUSED, True),
+        (PROJET_STATUS_ACCEPTED, PROJET_STATUS_DISMISSED, True),
+        (PROJET_STATUS_ACCEPTED, PROJET_STATUS_PROCESSING, True),
+        (PROJET_STATUS_REFUSED, PROJET_STATUS_REFUSED, False),
+        (PROJET_STATUS_REFUSED, PROJET_STATUS_DISMISSED, False),
+        (PROJET_STATUS_REFUSED, PROJET_STATUS_PROCESSING, False),
+        (PROJET_STATUS_DISMISSED, PROJET_STATUS_DISMISSED, False),
+        (PROJET_STATUS_DISMISSED, PROJET_STATUS_PROCESSING, False),
+        (PROJET_STATUS_PROCESSING, PROJET_STATUS_PROCESSING, False),
+    ],
+)
+def test_can_display_notification_tab_with_multiple_dotations(
+    first_dotation_status, second_dotation_status, expected_can_display_notification_tab
+):
+    projet = ProjetFactory()
+    DotationProjetFactory(
+        projet=projet, dotation=DOTATION_DETR, status=first_dotation_status
+    )
+    DotationProjetFactory(
+        projet=projet, dotation=DOTATION_DSIL, status=second_dotation_status
+    )
+    assert projet.can_display_notification_tab is expected_can_display_notification_tab
