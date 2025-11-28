d6de2681c1a6dc9492c2f3274dfee09ed7e8f98d
maximallain <maxime.allain.8@gmail.com>
2025-11-28 14:34:38 +0100

    fix(programmation): adapt tests

--- gsl_programmation/tests/test_urls.py
+++ gsl_programmation/tests/test_urls.py
@@ -18,7 +18,7 @@ def test_programmation_projet_list_dotation_url():
 def test_programmation_projet_detail_url():
     url = reverse(
         "gsl_programmation:programmation-projet-detail",
-        kwargs={"programmation_projet_id": 123},
+        kwargs={"projet_id": 123},
     )
     assert url == "/programmation/voir/123/"
 
@@ -31,6 +31,6 @@ def test_programmation_projet_tab_url(tab):
     """Test de l'URL des onglets"""
     url = reverse(
         "gsl_programmation:programmation-projet-tab",
-        kwargs={"programmation_projet_id": 123, "tab": tab},
+        kwargs={"projet_id": 123, "tab": tab},
     )
     assert url == f"/programmation/voir/123/{tab}/"
