bae90f747c7b5ba4de877d565ceba943423fe241
maximallain <maxime.allain.8@gmail.com>
2026-01-19 12:54:35 +0100

    refacto: use css class directly

--- gsl_core/static/js/controllers/formUtils.js
+++ gsl_core/static/js/controllers/formUtils.js
@@ -3,48 +3,18 @@ import { Controller } from 'stimulus'
 export class FormUtils extends Controller {
   static targets = ['form']
 
-  disableButton () {
-    const btn = this.element.querySelector('button[type="submit"]')
-    if (!btn) {
-      return
-    }
-
-    // Store original icon class if not already stored
-    if (!btn.dataset.originalIcon) {
-      // Find the icon class (fr-icon-*)
-      const iconClass = Array.from(btn.classList).find(
-        (cls) => cls.startsWith('fr-icon-') && !cls.includes('refresh')
-      )
-      if (iconClass) {
-        btn.dataset.originalIcon = iconClass
-      } else {
-        // Default icon if none found
-        btn.dataset.originalIcon = 'fr-icon-check-line'
-      }
-    }
-
-    // Replace icon with spinner (refresh icon)
-    if (btn.dataset.originalIcon) {
-      btn.classList.remove(btn.dataset.originalIcon)
-    }
-    btn.classList.add('fr-icon-refresh-line')
+  disableButton (evt) {
+    const btn = evt.target
+    btn.classList.add('fr-icon-loader')
     btn.classList.add('fr-icon-spin')
     btn.setAttribute('disabled', '1')
+    btn.setAttribute('data-action', 'form-utils#enableButton')
   }
 
-  enableButton (evt) {
-    const btn = this.element.querySelector('button[type="submit"]')
-    if (!btn) {
-      return
-    }
-
-    // Restore original icon
-    if (btn.dataset.originalIcon) {
-      btn.classList.remove('fr-icon-refresh-line', 'fr-icon-spin')
-      btn.classList.add(btn.dataset.originalIcon)
-    }
-
-    // Re-enable button
+  enableButton (evt) { // not used for now
+    const btn = evt.target
+    btn.classList.remove('fr-icon-loader', 'fr-icon-spin')
     btn.removeAttribute('disabled')
+    btn.setAttribute('data-action', 'form-utils#disableButton')
   }
 }
