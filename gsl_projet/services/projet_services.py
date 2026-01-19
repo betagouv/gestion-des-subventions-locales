f430f96dfc6c70b9036a9c16aad3e85ca1263ea1
maximallain <maxime.allain.8@gmail.com>
2026-01-19 09:13:20 +0100

    refacto(projet): move update dotations in form

--- gsl_projet/services/projet_services.py
+++ gsl_projet/services/projet_services.py
@@ -1,18 +1,10 @@
 import logging
 
-from django.db import transaction
 from django.db.models import Sum
 from django.db.models.query import QuerySet
 
-from gsl_core.models import Collegue
 from gsl_demarches_simplifiees.models import Dossier
-from gsl_demarches_simplifiees.services import DsService
-from gsl_projet.constants import (
-    POSSIBLE_DOTATIONS,
-    PROJET_STATUS_ACCEPTED,
-    PROJET_STATUS_PROCESSING,
-)
-from gsl_projet.models import Demandeur, DotationProjet, Projet
+from gsl_projet.models import Demandeur, Projet
 
 logger = logging.getLogger(__name__)
 
@@ -85,57 +77,6 @@ class ProjetService:
             dotation_projet__projet__in=projet_ids,
             status=ProgrammationProjet.STATUS_ACCEPTED,
         ).aggregate(total=Sum("montant"))["total"]
-
-    @classmethod
-    @transaction.atomic
-    def update_dotation(
-        cls, projet: Projet, dotations: list[POSSIBLE_DOTATIONS], user: Collegue
-    ):
-        from gsl_projet.services.dotation_projet_services import DotationProjetService
-
-        if len(dotations) == 0:
-            logger.warning(
-                "Projet must have at least one dotation", extra={"projet": projet.pk}
-            )
-            return
-        if len(dotations) > 2:
-            logger.warning(
-                "Projet can't have more than two dotations", extra={"projet": projet.pk}
-            )
-            return
-
-        new_dotations = set(dotations) - set(projet.dotations)
-        dotation_to_remove = set(projet.dotations) - set(dotations)
-
-        for dotation in new_dotations:
-            dotation_projet = DotationProjet.objects.create(
-                projet=projet, dotation=dotation, status=PROJET_STATUS_PROCESSING
-            )
-            DotationProjetService.create_simulation_projets_from_dotation_projet(
-                dotation_projet
-            )
-
-        dotation_projet_to_remove = DotationProjet.objects.filter(
-            projet=projet, dotation__in=dotation_to_remove
-        )
-
-        if dotation_projet_to_remove.filter(status=PROJET_STATUS_ACCEPTED).exists():
-            dotations_to_be_checked = (
-                DotationProjet.objects.filter(
-                    projet=projet, status=PROJET_STATUS_ACCEPTED
-                )
-                .exclude(dotation__in=dotation_to_remove)
-                .values_list("dotation", flat=True)
-            )
-
-            ds_service = DsService()
-            ds_service.update_ds_annotations_for_one_dotation(
-                dossier=projet.dossier_ds,
-                user=user,
-                dotations_to_be_checked=list(dotations_to_be_checked),
-            )
-
-        dotation_projet_to_remove.delete()
 
     # Private
 
