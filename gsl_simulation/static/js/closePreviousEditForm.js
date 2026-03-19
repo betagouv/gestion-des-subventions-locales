0d69e881fed732bf2e41a08027973964039e42f3
Maud Royer <hello@maudroyer.fr>
2026-03-19 11:12:46 +0100

    feat(js): enhance table edit form UX
    - Consolidate event handling logic into `tableEditForms.js`.
    - Add support for submitting edit forms with Ctrl+Enter or Cmd+Enter.
    - Update template to reference the new script.

--- gsl_simulation/static/js/closePreviousEditForm.js
+++
@@ -1,5 +0,0 @@
-document.body.addEventListener('htmx:beforeRequest', (evt) => {
-  if (evt.detail.elt.classList.contains('gsl-cell-edit-btn')) {
-    document.querySelectorAll('.gsl-cell-edit-form').forEach((el) => el.remove())
-  }
-})
