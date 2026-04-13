a15e49a5cf481664df59c3e5c09fbf0b6fb86162
Maud Royer <hello@maudroyer.fr>
2026-04-13 12:59:06 +0200

    feat(graphql): add configuration for DS GraphQL schema with authorization
    Signed-off-by: Maud Royer <hello@maudroyer.fr>

--- gsl_ds_proxy/apps.py
+++ gsl_ds_proxy/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class GslDsProxyConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "gsl_ds_proxy"
-    verbose_name = "3. Proxy DS"
+    verbose_name = "7. Proxy DS"
