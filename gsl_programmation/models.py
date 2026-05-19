9cabdd1c48f9ea5366fbe5da813f8bd6c39bc824
maximallain <maxime.allain.8@gmail.com>
2026-05-19 15:25:32 +0200

    fix: add or remove active()

--- gsl_programmation/models.py
+++ gsl_programmation/models.py
@@ -220,9 +220,7 @@ class ProgrammationProjetQuerySet(models.QuerySet):
     def visible_to_user(self, user):
         if user.is_staff:
             return self.all()
-        return self.filter(
-            dotation_projet__projet__in=Projet.objects.active().for_user(user)
-        )
+        return self.filter(dotation_projet__projet__in=Projet.objects.for_user(user))
 
 
 class ProgrammationProjetManager(
