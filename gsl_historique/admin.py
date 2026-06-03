dd01238e3c650d397c03e726d36f97a663d47ef0
maximallain <maxime.allain.8@gmail.com>
2026-06-03 15:04:31 +0200

    feat(admin): prevent adding ProjetAction via admin

--- gsl_historique/admin.py
+++ gsl_historique/admin.py
@@ -36,6 +36,16 @@ class ProjetActionAdmin(admin.ModelAdmin):
     )
     ordering = ("-created_at",)
 
+    def has_add_permission(self, request):
+        return False
+
+    def get_queryset(self, request):
+        return (
+            super()
+            .get_queryset(request)
+            .select_related("actor", "projet", "projet__dossier_ds", "enveloppe")
+        )
+
 
 @admin.register(CollegueLogEntry)
 class CollegueLogEntryAdmin(admin.ModelAdmin):
