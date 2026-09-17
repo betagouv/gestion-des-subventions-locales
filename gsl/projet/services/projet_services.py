c18dbb513a811ae80b33fd328040fb1950c8be3b
maximallain <maxime.allain.8@gmail.com>
2026-09-17 12:42:09 +0200

    feat(historique): create all ProjetAction from dossier traitements

--- gsl/projet/services/projet_services.py
+++ gsl/projet/services/projet_services.py
@@ -25,7 +25,6 @@ class ProjetService:
             projet = Projet(
                 dossier_ds=ds_dossier,
             )
-        was_created = projet.pk is None
         projet.address = ds_dossier.projet_adresse
         projet.is_in_qpv = cls._get_boolean_value(ds_dossier, "annotations_is_qpv")
         projet.is_attached_to_a_crte = cls._get_boolean_value(
@@ -48,13 +47,8 @@ class ProjetService:
         projet.contrat_local = ds_dossier.annotations_contrat_local
 
         projet.save()
-        if was_created and ds_dossier.ds_date_depot:
-            ProjetAction.objects.create(
-                projet=projet,
-                action_type=ProjetAction.TYPE_DEPOT_DOSSIER,
-                source=ProjetAction.SOURCE_DN,
-                created_at=ds_dossier.ds_date_depot,
-            )
+
+        ProjetAction.objects.create_from_dossier_traitements(projet)
         return projet
 
     # Private
